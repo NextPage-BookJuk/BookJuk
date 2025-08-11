9fbb737d0f3b2dc24ee3cc6ba3facaffe1b51b92
Kanggwanju <xprtktm1@gmail.com>
2025-08-12 01:26:53 +0900

    feat: MeetingRepository에 findByTitle 쿼리 메서드 추가

--- src/main/java/com/bookjuk/repository/meeting/MeetingRepository.java
+++ src/main/java/com/bookjuk/repository/meeting/MeetingRepository.java
@@ -3,5 +3,11 @@ package com.bookjuk.repository.meeting;
 import com.bookjuk.domain.meeting.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+
+    // 쿼리 메서드, 전달된 문자열과 제목명이 같은 모임을 찾음
+    List<Meeting> findByTitle(String title);
 }
