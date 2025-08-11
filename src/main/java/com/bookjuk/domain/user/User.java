5d50001eb6d60ea7c71f44ce5474a5694e61c863
Kanggwanju <xprtktm1@gmail.com>
2025-08-11 17:24:03 +0900

    feat: User 엔티티 Meeting과 MeetingParticipant 저장소 테스팅을 위해 임시로 필드 및 어노테이션 추가, 연관 관계 확장

--- src/main/java/com/bookjuk/domain/user/User.java
+++ src/main/java/com/bookjuk/domain/user/User.java
@@ -2,18 +2,35 @@ package com.bookjuk.domain.user;
 
 import com.bookjuk.domain.meeting.Meeting;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB의 AUTO_INCREMENT를 따릅니다.
     @Column(name = "user_id")
     private Long id;
 
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
     @OneToMany(mappedBy = "host", cascade = CascadeType.ALL, orphanRemoval = true)
     List<Meeting> meetings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "participant", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Meeting> participants = new ArrayList<>();
 }
