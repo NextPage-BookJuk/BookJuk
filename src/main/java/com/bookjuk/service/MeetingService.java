e1389b7c789a92502e75c00795dbb2a9ae1267fe
Kanggwanju <xprtktm1@gmail.com>
2025-08-12 16:37:29 +0900

    feat: MeetingService 및 MeetingController에 필수 어노테이션 및 의존성 추가

--- src/main/java/com/bookjuk/service/MeetingService.java
+++ src/main/java/com/bookjuk/service/MeetingService.java
@@ -1,4 +1,16 @@
 package com.bookjuk.service;
 
+import com.bookjuk.repository.meeting.MeetingRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MeetingService {
+
+    private final MeetingRepository meetingRepository;
 }
