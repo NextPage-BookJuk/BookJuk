aaada1d1dbc93f18d6033681234daafce7274ae0
minee0505 <wodeyue97@gmail.com>
2025-08-12 19:53:21 +0900

    feat: 원격에 있는  MeetingParticipantCustom 레포지토리 가져옴

---
+++ src/main/java/com/bookjuk/repository/participant/MeetingParticipantCustom.java
@@ -0,0 +1,11 @@
+package com.bookjuk.repository.participant;
+
+import com.bookjuk.domain.user.User;
+
+import java.util.List;
+
+public interface MeetingParticipantCustom {
+
+    // 미팅 id로 미팅 참여자 찾기
+    List<User> findMeetingParticipantsByMeetingId(Long id);
+}
