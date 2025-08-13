a81ab80192aefdba756c0eecbf2d0ebe813b8c47
Kanggwanju <xprtktm1@gmail.com>
2025-08-13 14:39:07 +0900

    feat: 모임 검색용 MeetingSearchRequest DTO 추가

---
+++ src/main/java/com/bookjuk/dto/meeting/MeetingSearchRequest.java
@@ -0,0 +1,18 @@
+package com.bookjuk.dto.meeting;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 모임 목록 조회용 검색 파라미터 DTO
+ * GET /api/meetings의 쿼리 파리미터 바인딩에 사용
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class MeetingSearchRequest {
+
+
+
+}
