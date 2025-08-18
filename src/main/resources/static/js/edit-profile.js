7b338591b13865651bba150b23b0328989df998e
dohee <doheeeq@gmail.com>
2025-08-18 17:51:46 +0900

    feat: 마이페이지 정보 조회 프론트엔드 구현 완료, 프로필 수정 프론트 엔드 구현

--- src/main/resources/static/js/edit-profile.js
+++ src/main/resources/static/js/edit-profile.js
@@ -1,1 +1,40 @@
+//=========== DOM ============//
+const
+<div className="profile-image" id="profileImage"></div>
 
+//=========== 렌더링 관련 함수 ============//
+const renderMyProfile = () => {
+
+    const user = getUserFromLocalStorage();
+
+}
+
+
+//=========== 기타 함수 ============//
+// 로컬 스토리지로부터 사용자 정보 가져오기
+function getUserFromLocalStorage() {
+    const user = localStorage.getItem('user');
+    return user;
+}
+
+//=========== 서버 데이터 요청/응답 관련 함수 ============//
+const updateMyProfile = async () => {
+
+}
+
+//=========== 메인 코드 실행 ============//
+(function () {
+
+    // 페이지가 처음 열렸을 때 로그인 상태를 확인
+    // 로그인 상태 확인
+    if (!checkLoginStatus()) {
+        alert('로그인이 필요한 서비스입니다.');
+        window.location.href = '/auth';
+        return;
+    }
+
+    updateMyProfile();
+
+    // 이벤트 핸들러들을 등록합니다.
+    // addEventListeners();
+})();
